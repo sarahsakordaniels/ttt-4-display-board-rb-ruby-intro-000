294e68b76deccd42d4341e52f13b43acebc2afc9
Sarah Daniels <ssdaniels13@gmail.com>
2019-01-22 05:57:52 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -10,8 +10,6 @@ def display_board(board)
   puts "   |   |   "
 end
 
-board = [" "," "," "," "," "," "," "," "," "]
-display_board(board)
 
 
 board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
